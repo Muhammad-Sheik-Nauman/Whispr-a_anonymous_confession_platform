0a54a02d95afffc0e89c171d742a81d1314c2118
Muhammad-Sheik-Nauman <noumansheik07@gmail.com>
2025-08-21 23:53:22 +0530

    style: update ConfessionForm and ConfessionList components for improved accessibility and visual consistency; adjust input and text colors

--- vite-project/src/ConfessionList.jsx
+++ vite-project/src/ConfessionList.jsx
@@ -46,16 +46,16 @@ const ConfessionList = ({ refresh, ownerToken }) => {
     );
 
     return (
-        <div className='max-w-2xl mx-auto p-4 bg-gray-900'>
+        <div className=' mx-auto p-4 bg-gray-800'>
             <input type="search" placeholder="Search keywords..." value={search} onChange={(e) => setSearch(e.target.value)} className='w-full mb-4 px-4 py-2 rounded-lg border border-gray-300 shadow-sm focus:outline-none focus:ring-2 focus:ring-green-500 bg-gray-800  placeholder-gray-400' />
             <div className='overflow-y-auto scroll-behavior-smooth max-h-[70vh] space-y-4' >
                 {filteredConfessions.length === 0 ? (
-                    <p className='text-black text-center'>No confessions available</p>
+                    <p className='text-gray-400 text-center'>No confessions available</p>
                 ) : (
                     filteredConfessions.map((confession) => (
                         <div key={confession._id}
-                        className="bg-white border border-gray-200 shadow rounded-lg p-4 " >
-                            <p className='text-black mb-2'><Highlighter
+                        className="bg-white border border-gray-200 shadow rounded-lg p-4 m-2 " >
+                            <p className='text-black mb-2 break-words'><Highlighter
 
                                 searchWords={[search]}
                                 autoEscape={true}
