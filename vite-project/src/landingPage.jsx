5372500e19a6538ef861df4f5e71263bd02adcd8
Muhammad-Sheik-Nauman <noumansheik07@gmail.com>
2025-08-21 22:33:46 +0530

    feat: integrate Tailwind CSS and update assets
    - Added Tailwind CSS as a dependency and configured it in vite.config.js.
    - Removed old logo.jpeg and vite.svg files, replacing them with logo.png.
    - Updated App.css and index.css to use Tailwind CSS for styling.
    - Enhanced landingPage.jsx with Tailwind CSS classes for improved UI.

--- vite-project/src/landingPage.jsx
+++ vite-project/src/landingPage.jsx
@@ -6,17 +6,31 @@ import { useNavigate } from 'react-router-dom';
 const LandingPage = () => {
   const navigate = useNavigate();
 
-  
+
 
   return (
-    <div>
-        <img src="logo.jpeg" alt="Logo" height={150}/>
-        <button onClick={
-            () => navigate('/confessions')
-        }>get started</button>
+    <>
+      <div className="min-h-screen bg-gradient-to-b from-gray-900 via-gray-800 to-black text-white flex flex-col items-center justify-center px-6">
+        <img
+          src="logo.png"
+          alt="Logo"
+          className="w-100 h-100  rounded-full invert"
 
-    </div>
+        />
+
+        <h1 className='text-5xl font-bold mb-3 font-sans '>WHISPR</h1>
+        <p className='text-gray-400 uppercase tracking-widest mb-10'>ADMIT. RELEASE. RENEW</p>
+        
+        <button onClick={
+          () => navigate('/confessions')
+        } className="px-8 py-3 bg-gradient-to-r from-green-500 to-emerald-600 rounded-full text-lg font-medium shadow-lg hover:scale-105 transform transition duration-300"
+        >Get Started</button>
+        
+
+      </div>
+    </>
   )
 }
 
 export default LandingPage;
+
